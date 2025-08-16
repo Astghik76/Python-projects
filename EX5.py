3d95345e7036455dd6d16d0fdc7c252ee2c88b63
Astx Janinyan <janinyanastx@gmail.com>
2025-08-16 16:36:09 +0400

    Add EX5.py python-exercise

--- EX5.py
+++ EX5.py
@@ -1,9 +1,18 @@
-num1 = input("Enter number1 ") 
-num2 = input("Enter number2 ")
+num1 = int(input("Enter number1 "))
+num2 = int(input("Enter number2 "))
 
-num1 = int(num1)
-num2 = int(num2)
+add_result = num1 + num2
+diff_result = num1 - num2 
+prod_result = num1 * num2
+div_result =  num1 / num2
+floor_div_result = num1 // num2
+mod_result = num1 % num2
+power_result = num1 ** num2
 
-result = num1 + num2
-
-print("The sum is ", result)
+print("The sum is ", add_result)
+print("The diff is ", diff_result)
+print("The prod is ", prod_result)
+print("The div is ", div_result)
+print("The floor_div is ", floor_div_result)
+print("The mod is ", mod_result)
+print("The power is ", power_result)
