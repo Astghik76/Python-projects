dbf1cf003bbb44054eb281fc3dce712d7dd2cc72
Astx Janinyan <janinyanastx@gmail.com>
2025-08-16 15:42:51 +0400

    Add EX3.py python-exercise

---
+++ EX3.py
@@ -0,0 +1,9 @@
+name = "Astghik"
+age = 17
+height = 1.63
+I_student = True 
+ 
+print("Type of name ", type(name))
+print("Type of age ", type(age))
+print("Type of height ", type(height))
+print("Type of I_student ", type(I_student))
