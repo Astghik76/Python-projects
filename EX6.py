d443bb8a28eb92a3c5390813d815eab39e3c811e
Astx Janinyan <janinyanastx@gmail.com>
2025-08-18 14:18:33 +0400

    Add EX6.py python-exercise

---
+++ EX6.py
@@ -0,0 +1,8 @@
+number = int(input("Enter number "))
+ 
+if number > 0:
+  print(str(number) + "ը" + " դրական է։")
+elif number < 0:
+ print(str(number) + "ը" + " բացասական է:")
+else:
+  print(str(number) + "ը" + " զրո է։")
