d41d8dc7db09918e2e869f95e46f8ab5ec3bc22c
Astx Janinyan <janinyanastx@gmail.com>
2025-08-21 22:43:46 +0400

    Add EX10.py python-exercise

---
+++ EX10.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3] 
+my_list.insert(1, 9)
+my_list.append(2)
+my_list.extend([5, 10, 11])
+print(my_list)
+
+my_list2 = [4, 5, 6, 8, 22]
+my_list2.remove(4)
+my_list2.pop(1)
+my_list2.clear()
+print(my_list2)
